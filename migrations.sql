c12ab87da29cbaee6a8830fdda15afd4d4ec6d46
brian-moretti <brianmoretti2512@gmail.com>
2024-03-06 11:03:17 +0100

    Fix the structure of the project. READMe completed and removed tests

--- migrations.sql
+++ migrations.sql
@@ -3,6 +3,8 @@ CREATE TABLE `goal`(
     `title` VARCHAR(255) NOT NULL,
     `description` VARCHAR(255) NOT NULL
 );
+ALTER TABLE
+    `goal` ADD UNIQUE `goal_title_unique`(`title`);
 CREATE TABLE `users`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
